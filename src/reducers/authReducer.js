1f8b1a44a09c475a5a0129619a2e446e0a8db7d9
kdaglas <daglach7@gmail.com>
2019-03-13 03:58:58 +0300

    feat(login): enable an attendant or admin to login into the app
    - add the login components
    - add the auth reducer,
    - create the login view
    - add a font to be used
    
    [Starts #164530011]

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,30 @@
+import ActionTypes from '../actions/ActionTypes';
+
+const initialState = {
+  message: '',
+  token: '',
+  isSuccessful: false,
+  error: '',
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionTypes.LOGIN_SUCCESS:
+      return {
+        ...state,
+        isSuccessful: true,
+        message: action.payload.themessage,
+        token: action.payload.token
+      };
+      case ActionTypes.LOGIN_FAILED:
+      return {
+        ...state,
+        isSuccessful: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
